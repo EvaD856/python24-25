790eb7bef21accc132ad35c20058ddf78c605a55
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2024-09-26 14:55:54 +0000

    if/else statements

---
+++ ifStatements.py
@@ -0,0 +1,8 @@
+lower = int(input("Enter a lower number"))
+upper = int(input("Enter a lower number"))
+
+if upper is int():
+    print("They entered the correct data.")
+
+if lower <= upper:
+    print("The Statement is True.")
