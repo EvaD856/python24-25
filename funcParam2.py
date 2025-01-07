daba24a6d156a0551316d392b60feb9efefa2e0e
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2025-01-07 15:16:18 +0000

    tuesday practice

--- funcParam2.py
+++ funcParam2.py
@@ -10,10 +10,25 @@ def myFunction(a , b):
     print("\n", a, "to the power of ", b, "is: ", exp1)
     print("\n", b, "to the power of ", a, "is: ", exp1)
 
-
+def sum(first, second):
+    total = first + second
+    print("The total of your data input is:", total)
 
 # Ask the user for 2 integers between 1 and 10
 a = int(input("\nPlease input a number between 1 and 10: "))
 b = int(input("\nPlease enter a number between 1 and 10: "))
 
 myFunction(a, b)
+
+
+print("\n Part 2 of our Example:\n")
+
+
+#Give the user the option to repeat this process as many times as they want.
+#If they want to quit, they type the word Quit
+    # Have users input positive integers
+myVar1 = int(input("Please Enter a positive whole number or type Quit to exit"))
+myVar2 = int(input("Please Enter a positive whole number "))
+
+# call the sum() function to 
+sum(myVar1, myVar2)
